8f2e24fb5fe3094b82e036c9f8963778f668fcc8
Dani Rey <dani.rey.ch@gmail.com>
2018-12-18 00:08:31 +0100

    show named/anonymous fun

--- src/App/filterAndMap.fsx
+++ src/App/filterAndMap.fsx
@@ -3,6 +3,18 @@
 open Library
 open Shopping
 
+let itProducts = belongsToCategory it
+
+let nameProjection(p: Product) = p.Name
+
+printfn("Names of IT products in basket (built with named functions)")
+basket 
+  |> List.filter(itProducts)
+  |> List.map(nameProjection)
+  |> List.iter(printfn("%s"))
+
+printfn("")
+printfn("Names of IT products in basket (built with anonymous functions)")
 basket 
   |> List.filter(belongsToCategory it)
   |> List.map(fun p -> p.Name)
